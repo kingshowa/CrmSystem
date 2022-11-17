f30906704df8a6857672f3660b5b70067b6c5764
kingshowa <kingshowa1@gmail.com>
2022-11-17 19:03:10 +0100

    Update Master.blade.php

--- resources/views/layouts/Master.blade.php
+++ resources/views/layouts/Master.blade.php
@@ -76,7 +76,7 @@
 
           <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
             <li class="dropdown-header">
-              <h6>Kevin Anderson</h6>
+              <h6>Kevin Peterson</h6>
               <span>Web Designer</span>
             </li>
             <li>
