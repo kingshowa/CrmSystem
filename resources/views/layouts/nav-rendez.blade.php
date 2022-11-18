da2c194fa75193266320d4d994f83e433ca27477
kingshowa <kingshowa1@gmail.com>
2022-11-18 14:53:54 +0100

    nav files added

---
+++ resources/views/layouts/nav-rendez.blade.php
@@ -0,0 +1,66 @@
+@extends("layouts.Master")
+
+@Section("content")
+
+<!-- ======= Sidebar For contact ======= -->
+<aside id="sidebar" class="sidebar">
+
+<ul class="sidebar-nav" id="sidebar-nav">
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="{{ url('admin')}}">
+      <i class="bi bi-grid"></i>
+      <span>Dashboard</span>
+    </a>
+  </li><!-- End Dashboard Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="{{ url('prospects')}}">
+      <i class="bi bi-person-plus-fill"></i><span>Prospects</span>
+    </a>
+    
+  </li><!-- End Prospects Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="{{ url('contacts')}}">
+      <i class="bi bi-person-lines-fill"></i><span>Contacts</span>
+    </a>
+  </li><!-- End Contacts Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="{{ url('clients')}}">
+      <i class="bi bi-person-check-fill"></i><span>Clients</span>
+    </a>
+  </li><!-- End Clients Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="{{ url('opportunites')}}">
+      <i class="bi bi-bar-chart"></i><span>Oppotunites</span>
+    </a>
+  </li><!-- End Oppotunites Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="{{ url('produits')}}">
+      <i class="bi bi-gem"></i><span>Produits</span>
+    </a>
+  </li><!-- End Products Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="{{ url('utilisateurs')}}">
+      <i class="bi bi-person"></i>
+      <span>Utilisateurs</span>
+    </a>
+  </li><!-- End Users Nav -->
+  <li class="nav-item">
+    <a class="nav-link" href="{{ url('rendez-vous')}}">
+      <i class="bi bi-envelope"></i>
+      <span>Rendez-Vous</span>
+    </a>
+  </li>
+</ul>
+
+</aside><!-- End Sidebar-->
+
+@yield("rendez-vous")
+
+@endsection
