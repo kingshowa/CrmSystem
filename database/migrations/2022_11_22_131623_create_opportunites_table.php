7b4c4bfa31a9371ae50e3dd19f8e7a0c09fb1f81
IsmailaSIDIBE <ismailahayoo73@gmail.com>
2022-11-22 14:37:49 +0100

    i

---
+++ database/migrations/2022_11_22_131623_create_opportunites_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('opportunites', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->string('montant');
+            $table->string('etapes');
+            $table->string('date_clôture');
+            $table->string('client');
+            $table->string('produits');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('opportunites');
+    }
+};
