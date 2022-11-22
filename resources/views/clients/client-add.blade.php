d96264fbd39d7b499b0196e4907bbbf35c6954dc
hanane <hanannina968@gmail.com>
2022-11-22 13:45:04 +0100

    hanane

--- resources/views/clients/client-add.blade.php
+++ resources/views/clients/client-add.blade.php
@@ -23,19 +23,20 @@
           <div class="card"> 
             <div class="card-body pt-3 col-xl-8">
 
-              <form action="{{url('client/store')}}" methode="post">
+              <form action="{{url('client/store')}}" methode="POST">
+              {{ csrf_field()}}
 
                     <div class="row mb-3">
                       <label for="firstName" class="col-md-4 col-lg-3 col-form-label">First Name</label>
                       <div class="col-md-8 col-lg-9">
-                        <input name="firstName" type="text" class="form-control" id="firstName">
+                        <input name="societe" type="text" class="form-control" id="firstName">
                       </div>
                     </div>
 
                     <div class="row mb-3">
                       <label for="surName" class="col-md-4 col-lg-3 col-form-label">Surname</label>
                       <div class="col-md-8 col-lg-9">
-                        <input name="surName" type="text" class="form-control" id="surName">
+                        <input name="telephone" type="text" class="form-control" id="surName">
                       </div>
                     </div>
 
@@ -43,24 +44,16 @@
                     <div class="row mb-3">
                       <label for="about" class="col-md-4 col-lg-3 col-form-label">Address</label>
                       <div class="col-md-8 col-lg-9">
-                        <textarea name="address" class="form-control" id="about" style="height: 100px">Type Your Address ...</textarea>
+                        <textarea name="adresse" class="form-control" id="about" style="height: 100px">Type Your Address ...</textarea>
                       </div>
                     </div>
 
                     <div class="row mb-3">
                       <label for="Phone" class="col-md-4 col-lg-3 col-form-label">Phone</label>
                       <div class="col-md-8 col-lg-9">
-                        <input name="phone" type="text" class="form-control" id="Phone">
+                        <input name="site_web" type="text" class="form-control" id="Phone">
                       </div>
                     </div>
-
-                    <div class="row mb-3">
-                      <label for="Email" class="col-md-4 col-lg-3 col-form-label">Site Web</label>
-                      <div class="col-md-8 col-lg-9">
-                        <input name="email" type="email" class="form-control" id="Email">
-                      </div>
-                    </div>
-
                     <div class="text-center">
                       <button type="submit" class="btn btn-primary">Save Client</button>
                     </div>
@@ -74,24 +67,6 @@
       </div>
     </section>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
   </main><!-- End #main -->
 
-  
 @endsection;
