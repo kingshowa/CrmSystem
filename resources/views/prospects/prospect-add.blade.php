36bc595b40f43cb19cfffd746d513163aecc6463
IsmailaSIDIBE <ismailahayoo73@gmail.com>
2022-11-24 21:21:57 +0100

    i

--- resources/views/prospects/prospect-add.blade.php
+++ resources/views/prospects/prospect-add.blade.php
@@ -23,49 +23,51 @@
           <div class="card"> 
             <div class="card-body pt-3 col-xl-8">
 
-              <form action="{{ route('store_opportunite') }}" method="POST">
+              <form action="{{ route('store_prospect') }}" method="POST">
 
               {{ csrf_field()}}
 
 
 
                     <div class="row mb-3">
-                      <label for="firstName" class="col-md-4 col-lg-3 col-form-label">nom</label>
+                      <label for="firstName" class="col-md-4 col-lg-3 col-form-label">Nom</label>
                       <div class="col-md-8 col-lg-9">
                         <input name="nom" type="text" class="form-control" id="firstName" require>
                       </div>
                     </div>
 
                     <div class="row mb-3">
-                      <label for="surName" class="col-md-4 col-lg-3 col-form-label">prenom</label>
+                      <label for="surName" class="col-md-4 col-lg-3 col-form-label">Prenom</label>
                       <div class="col-md-8 col-lg-9">
                         <input name="prenom" type="text" class="form-control" id="surName" require>
                       </div>
                     </div>
 
                     <div class="row mb-3">
-                      <label for="company" class="col-md-4 col-lg-3 col-form-label">societe</label>
+                      <label for="company" class="col-md-4 col-lg-3 col-form-label">Societe</label>
                       <div class="col-md-8 col-lg-9">
                         <input name="societe" type="text" class="form-control" id="company" require>
                       </div>
                     </div>
 
                     <div class="row mb-3">
-                      <label for="Job" class="col-md-4 col-lg-3 col-form-label">fonction</label>
+                      <label for="Job" class="col-md-4 col-lg-3 col-form-label">Fonction</label>
                       <div class="col-md-8 col-lg-9">
                         <input name="fonction" type="text" class="form-control" id="Job" require>
                       </div>
                     </div>
 
+
                     <div class="row mb-3">
-                      <label for="about" class="col-md-4 col-lg-3 col-form-label">Adresse</label>
+                      <label for="address" class="col-md-4 col-lg-3 col-form-label">Adresse</label>
                       <div class="col-md-8 col-lg-9">
-                        <textarea name="adresse" class="form-control" id="about" style="height: 100px" require>Type Your Address ...</textarea>
+                        <textarea name="adresse" class="form-control" id="address" style="height: 100px">add your adresse</textarea>
                       </div>
                     </div>
 
+
                     <div class="row mb-3">
-                      <label for="Phone" class="col-md-4 col-lg-3 col-form-label">telephone</label>
+                      <label for="Phone" class="col-md-4 col-lg-3 col-form-label">Telephone</label>
                       <div class="col-md-8 col-lg-9">
                         <input name="telephone" type="text" class="form-control" id="Phone" require>
                       </div>
@@ -78,8 +80,30 @@
                       </div>
                     </div>
 
+
+                    <div class="row mb-3">
+                      <label for="about" class="col-md-4 col-lg-3 col-form-label">Site Web</label>
+                      <div class="col-md-8 col-lg-9">
+                        <input name="site_web" class="form-control" id="about" text="text" require>
+                      </div>
+                    </div>
+
+                    <div class="row mb-3">
+                      <label for="about" class="col-md-4 col-lg-3 col-form-label">Source</label>
+                      <div class="col-md-8 col-lg-9">
+                        <input name="source" class="form-control" id="sources" text="text" require>
+                      </div>
+                    </div>
+
+                    <div class="row mb-3">
+                      <label for="about" class="col-md-4 col-lg-3 col-form-label">Statut</label>
+                      <div class="col-md-8 col-lg-9">
+                        <input name="statut" class="form-control" id="statuts" text="text" require>
+                      </div>
+                    </div>
+
                     <div class="text-center">
-                      <button type="submit" class="btn btn-primary">Save Contact</button>
+                      <button type="submit" class="btn btn-primary">Save prospect</button>
                     </div>
                   </form><!-- End Profile Edit Form -->
 
